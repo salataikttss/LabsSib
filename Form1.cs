b9142b9fbe3cbb216cc0707b86bef431a097f393
salataikttss <sibgatovy@gmail.com>
2026-04-07 09:55:26 +0500

    add 7 lab

--- Form1.cs
+++ Form1.cs
@@ -1,117 +1,70 @@
-using System;
-using System.Windows.Forms;
+namespace sib_application;
 
-namespace app_31._03._26_winforms
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        private TextBox txtHeight;
-        private TextBox txtPressure;
-        private Button btnCalculate;
-        private Label lblResult;
-        private Label lblHeight;
-        private Label lblPressure;
+        InitializeComponent();
 
-        public Form1()
+        Label label1 = new Label();
+        label1.Name = "testLabel1";
+        label1.Location = new Point(50, 50);
+        label1.Size = new Size { Width = 300, Height = 30 };
+
+        Label label2 = new Label();
+        label2.Name = "testLabel2";
+        label2.Location = new Point(50, 90);
+        label2.Size = new Size { Width = 370, Height = 30 };
+
+        Label label3 = new Label();
+        label3.Name = "testLabel3";
+        label3.Location = new Point(50, 130);
+        label3.Size = new Size { Width = 300, Height = 30 };
+
+        Label label4 = new Label();
+        label4.Name = "testLabel4";
+        label4.Location = new Point(50, 170);
+        label4.Size = new Size { Width = 300, Height = 30 };
+
+        Label label5 = new Label();
+        label5.Name = "testLabel5";
+        label5.Location = new Point(50, 210);
+        label5.Size = new Size { Width = 300, Height = 30 };
+
+        unsafe
         {
-            InitializeComponent();
-            InitializeCustomComponents();
+            // variable1
+            char sym = '*';
+            char* ptr1 = &sym;
+            ulong addr1 = (ulong)ptr1;
+            label1.Text = $"variable sym: value = {sym}, address = {addr1}";
+
+            sym = '/';
+            label2.Text = $"variable sym ����� ���������: value = {sym}, address = {addr1}";
+
+            // variable2
+            int number = 42;
+            int* ptr2 = &number;
+            ulong addr2 = (ulong)ptr2;
+            label3.Text = $"viraible number: value = {number}, address = {addr2}";
+
+            // variable3
+            double pi = 3.14159;
+            double* ptr3 = &pi;
+            ulong addr3 = (ulong)ptr3;
+            label4.Text = $"viraible pi: value = {pi}, address = {addr3}";
+
+            // variable4
+            bool flag = true;
+            bool* ptr4 = &flag;
+            ulong addr4 = (ulong)ptr4;
+            label5.Text = $"viraible flag: value = {flag}, address = {addr4}";
         }
 
-        private void InitializeCustomComponents()
-        {
-            this.Text = "Расчет плотности жидкости";
-            this.Size = new System.Drawing.Size(400, 300);
-
-            lblHeight = new Label();
-            lblHeight.Text = "Столб жидкости высотой (см):";
-            lblHeight.Location = new System.Drawing.Point(20, 20);
-            lblHeight.Size = new System.Drawing.Size(200, 25);
-
-            txtHeight = new TextBox();
-            txtHeight.Location = new System.Drawing.Point(220, 20);
-            txtHeight.Size = new System.Drawing.Size(150, 20);
-
-            lblPressure = new Label();
-            lblPressure.Text = "Давление в паскалях:";
-            lblPressure.Location = new System.Drawing.Point(20, 60);
-            lblPressure.Size = new System.Drawing.Size(200, 25);
-
-            txtPressure = new TextBox();
-            txtPressure.Location = new System.Drawing.Point(220, 60);
-            txtPressure.Size = new System.Drawing.Size(150, 20);
-
-            btnCalculate = new Button();
-            btnCalculate.Text = "Рассчитать плотность";
-            btnCalculate.Location = new System.Drawing.Point(20, 100);
-            btnCalculate.Size = new System.Drawing.Size(350, 30);
-            btnCalculate.Click += BtnCalculate_Click;
-
-            lblResult = new Label();
-            lblResult.Text = "Плотность: ";
-            lblResult.Location = new System.Drawing.Point(20, 150);
-            lblResult.Size = new System.Drawing.Size(350, 50);
-            lblResult.Font = new System.Drawing.Font("Arial", 10, System.Drawing.FontStyle.Bold);
-
-            this.Controls.Add(lblHeight);
-            this.Controls.Add(txtHeight);
-            this.Controls.Add(lblPressure);
-            this.Controls.Add(txtPressure);
-            this.Controls.Add(btnCalculate);
-            this.Controls.Add(lblResult);
-        }
-
-        private void BtnCalculate_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                double h = Convert.ToDouble(txtHeight.Text); // высота столба жидкости в сантиметрах
-                double p = Convert.ToDouble(txtPressure.Text); // давление в паскалях
-
-                h = h / 100;
-
-                double g = 9.8; // ускорение свободного падения в м/с^3
-
-                // Formula: p = density * g * h, therefore density = p / (g * h)
-                double density = p / (g * h);
-
-                lblResult.Text = $"Плотность жидкости: {density} кг/м^3";
-
-                if (density < 0)
-                {
-                    lblResult.Text += "\n(Ошибка: отрицательная плотность)";
-                }
-                else if (density > 20000)
-                {
-                    lblResult.Text += "\n(Внимание: нереалистично высокая плотность)";
-                }
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Пожалуйста, введите корректные числовые значения!",
-                    "Ошибка ввода",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Warning);
-            }
-            catch (DivideByZeroException)
-            {
-                MessageBox.Show("Высота столба не может быть равна нулю!",
-                    "Ошибка расчета",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Warning);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Произошла ошибка: {ex.Message}",
-                    "Ошибка",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
-            }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-        //.
-        }
+        this.Controls.Add(label1);
+        this.Controls.Add(label2);
+        this.Controls.Add(label3);
+        this.Controls.Add(label4);
+        this.Controls.Add(label5);
     }
 }
